b4113315d4f5e3bda2ace6721b58bcb7ca717d76
helkv <helk@mpdl.mpg.de>
2018-07-11 17:28:08 +0200

    Handle already logged out users in logout-mehod

--- src/main/java/components/MetaMenuComponent.java
+++ src/main/java/components/MetaMenuComponent.java
@@ -1,24 +1,30 @@
 package main.java.components;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.testng.log4testng.Logger;
 
-import main.java.pages.LoginPage;
 import main.java.pages.StartPage;
 
 /**
- * Menu containing login, contact, disclaimer, help, and language choice
+ * Menu containing: login/logout, contact, disclaimer, privacy policy, guide, help, and language choice
  * @author apetrova
  *
  */
+//TODO: Merge with LoginPage
 public class MetaMenuComponent {
 
 	protected WebDriver driver;
+	protected final Logger log4j = Logger.getLogger(MetaMenuComponent.class);
+	
+	@FindBy(xpath = "//input[contains(@id, 'lnkLogin')]")
+	private WebElement loginButton;
 	
 	@FindBy(xpath = "//input[contains(@id, 'lnkLogout')]")
-	private WebElement loginButton;
+	private WebElement logoutButton;
 	
 	public MetaMenuComponent(WebDriver driver) {
 		this.driver = driver;
@@ -26,16 +32,14 @@ public class MetaMenuComponent {
 		PageFactory.initElements(driver, this);
 	}
 	
-	public LoginPage goToLoginPage() {
-		loginButton.click();
-		
-		return PageFactory.initElements(driver, LoginPage.class);
-	}
-	
 	public StartPage logout() {
-		loginButton.click();
-		
-		return PageFactory.initElements(driver, StartPage.class);
+		if(driver.findElements(By.xpath("//input[contains(@id, 'lnkLogin')]")).isEmpty()) {
+			logoutButton.click();		
+			return PageFactory.initElements(driver, StartPage.class);
+		}else {
+			log4j.error("User already logged out.");
+			return PageFactory.initElements(driver, StartPage.class);
+		}		
 	}
 	
 }
