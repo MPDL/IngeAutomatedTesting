9c7a06c6334fc38f59314341fa1ac1d2c57638b2
helkv <helk@mpdl.mpg.de>
2019-01-08 10:19:46 +0100

    Added own ExpectedCondition-method newWindowOpened
    - Added new method newWindowOpened() to SeleniumWrapper.
    -- It returns an ExpectedCondition and can be used in wait.until()
    -- It should be used to check weather a new window has been opened

--- src/test/java/base/SeleniumWrapper.java
+++ src/test/java/base/SeleniumWrapper.java
@@ -3,6 +3,7 @@ package test.java.base;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedCondition;
 
 /**
  * 
@@ -34,6 +35,30 @@ public class SeleniumWrapper {
 	public static void hideWebElement(WebDriver driver, WebElement webElement) {
 		JavascriptExecutor jse = (JavascriptExecutor) driver;
 		jse.executeScript("arguments[0].style.visibility = 'hidden';",  webElement);
+	}
+	
+	/**
+	 * Implementation of an own ExpectedCondition. <br>
+	 * An expectation for checking that the number of windows has increased by one.
+	 * 
+	 * @param oldWindowCount the number of windows before opening a new window
+	 * @return <code>true</code> when a new window is opened.
+	 */
+	public static ExpectedCondition<Boolean> newWindowOpened(final int oldWindowCount){
+		return new ExpectedCondition<Boolean>() {
+		
+	      @Override
+	      public Boolean apply(WebDriver webDriver) {
+	    	  int currentWindowCount = webDriver.getWindowHandles().size();
+	    	  
+	    	  return currentWindowCount == oldWindowCount + 1;
+	      }
+	
+	      @Override
+	      public String toString() {
+	    	  return "number of windows = " + oldWindowCount + " + 1";
+	      }
+	   };
 	}
 	
 }
