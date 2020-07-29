1097af3dbdcd8fed5d849f71cc37a86cd78062c0
helkv <helk@mpdl.mpg.de>
2020-07-29 08:31:52 +0200

    Added another wait to MetaMenuComponent
    - Added wait for page load complete after logout

--- src/main/java/components/MetaMenuComponent.java
+++ src/main/java/components/MetaMenuComponent.java
@@ -1,6 +1,7 @@
 package main.java.components;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -12,7 +13,9 @@ import org.testng.log4testng.Logger;
 import main.java.pages.StartPage;
 
 /**
- * Menu containing: login/logout, contact, disclaimer, privacy policy, guide, help, and language choice
+ * Menu containing: login/logout, contact, disclaimer, privacy policy, guide,
+ * help, and language choice
+ * 
  * @author apetrova
  *
  */
@@ -21,32 +24,34 @@ public class MetaMenuComponent {
 
 	protected WebDriver driver;
 	protected final Logger log4j = Logger.getLogger(MetaMenuComponent.class);
-	
+
 	@FindBy(xpath = "//input[contains(@id, 'lnkLogin')]")
 	private WebElement loginButton;
-	
+
 	@FindBy(xpath = "//input[contains(@id, 'lnkLogout')]")
 	private WebElement logoutButton;
-	
+
 	public MetaMenuComponent(WebDriver driver) {
 		this.driver = driver;
-		
+
 		PageFactory.initElements(driver, this);
 	}
-	
+
 	// PM-91, PM-92
 	public StartPage logout() {
-		if(driver.findElements(By.xpath("//input[contains(@id, 'lnkLogin')]")).isEmpty()) {
+		if (driver.findElements(By.xpath("//input[contains(@id, 'lnkLogin')]")).isEmpty()) {
 			WebElement logoutButton = driver.findElement(By.xpath("//input[contains(@id, 'lnkLogout')]"));
 			logoutButton.click();
-			
+
 			new WebDriverWait(driver, 20).until(ExpectedConditions.stalenessOf(logoutButton));
-			
+			new WebDriverWait(driver, 20).until(webDriver -> ((JavascriptExecutor) webDriver)
+					.executeScript("return document.readyState").equals("complete"));
+
 			return PageFactory.initElements(driver, StartPage.class);
-		}else {
+		} else {
 			log4j.error("User already logged out.");
 			return PageFactory.initElements(driver, StartPage.class);
-		}		
+		}
 	}
-	
+
 }
